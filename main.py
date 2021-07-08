b742eb76985f5e24836dc4c76178b0eca5b2fc36
Rizel Tane <rizelita@gmail.com>
2021-07-08 01:08:34 -0400

    Extracting repositories information from user.

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-from pprint import pprint
+import requests
+
 from playwright.sync_api import sync_playwright
+from bs4 import BeautifulSoup
+from pprint import pprint
 
 from retriever import obtain_data
 from parser import parse_informational_data
@@ -14,8 +17,6 @@ global interesting_people
 for job_title in INTERESTING_JOB_TITLES:
     interesting_people[job_title] = []
 
-youtube = None
-
 
 def print_obtained_data():
     pprint(argyle_countries)
@@ -28,18 +29,43 @@ def print_obtained_data():
 
 
 
-def process_user(context, username, user_index):
+def process_user(context, username, about_user, user_index):
     users_page_url  = f"https://github.com/{username}?tab=repositories"
     users_page      = context.new_page()
     users_page.goto(users_page_url)
     users_page.screenshot(path=f"repositories_of_{username}.png")
     users_page.close()
+
     # process webpage repositories content
+    response    = requests.get(users_page_url)
+    parsed_tree = BeautifulSoup(response.text, 'html.parser')
+    image_url               = parsed_tree.find_all("img", class_="rounded-1 avatar-user")[0]['src']
+    number_of_repositories  = int(parsed_tree.find(attrs={"class":"Counter"}).string)
+    location                = parsed_tree.find("li" , attrs={"itemprop":"homeLocation"}).string
+    print(image_url)
+    print(number_of_repositories)
+    print(about_user)
+    # more urls
+    rel=""
+    url_tags = parsed_tree.find_all(attrs={"class":"Link--primary "})
+    print(url_tags)
+    for url_tag in url_tags:
+        print(url_tag)
+        print(url_tag['href'])
+
+    repositories = parsed_tree.find_all(attrs={"itemprop":"name codeRepository"})
+    for repo in repositories:
+        print(repo.string)
+    repositories_descriptions = parsed_tree.find_all(attrs={"itemprop":"description"})
+    pprint(repositories_descriptions)
+    repositories_languages = parsed_tree.find_all(attrs={"itemprop":"programmingLanguage"})
+    pprint(repositories_languages)
 
 
 # this function executed with at least one user in the search result.
 def evaluate_each_user(context, page, total_users):
     username    = page.eval_on_selector( f":nth-match(a.color-text-secondary, 1)", 'e => e.textContent')
+    about_user  = None
     user_found  = False
 
     for user_index in range(1,total_users+1):
@@ -50,18 +76,17 @@ def evaluate_each_user(context, page, total_users):
         # try to match the user.
         if 'argyle' in about_user:
             user_found  = True
-            user_data   = process_user(context, username, user_index)
+            user_data   = process_user(context, username, about_user, user_index)
         page.wait_for_load_state('networkidle')
 
     if not user_found:
         # process the first user
-        user_data   = process_user(context, username, 1)
+        user_data   = process_user(context, username, about_user, 1)
         
 
 
 
-def interact_with_github():
-    name = 'Lucas Queiroz'
+def interact_with_github(name):
     github_fullname = f"fullname:{name}"
     title = f"Search · fullname:{name} · GitHub"
     total_users = 0
@@ -93,16 +118,19 @@ def interact_with_github():
 
 
 def find_in_github():
-    for person in interesting_people:
-        None
+    interact_with_github(name)
+
+    '''
+    for job_title in interesting_people:
+        for person in job_title:
+            interact_with_github(person['Name'])
+    '''
+
 
         
-def find_in_youtube():
-    None
 
 
 # obtain_data()
-# parse_informational_data()
-# print_obtained_data()
-# find_in_github()
-interact_with_github()
+parse_informational_data()
+print_obtained_data()
+find_in_github()
